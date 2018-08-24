ba4bb2b21aff7faf7443e4a3d15dcd3be81af1f3
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-24 09:37:03 -0700

    :

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+//Back End Logic
+
+var Toppings //[topping, priceMultiplier]
+  ["extraCheese", 1],
+  ["pepperoni", 1],
+  ["sausage", 1],
+  ["salami", 1],
+  ["canadianBacon", 1],
+  ["bacon", 1],
+  ["chicken", 1],
+  ["anchovies", 1],
+  ["pineapple", 0.5],
+  ["mushrooms", 0.5],
+  ["olives", 0.5],
+  ["peppers", 0.5],
+  ["onions", 0.5]
+]
+
+function Pizza() {
+  this.size = NaN;
+  this.crust = "";
+  this.toppings = [];
+  this.extraSauce = false;
+};
+
+function Order() {
+  this.pizza = [];
+  this.price = NaN;
+  this.delivery = false;
+};
+
+Pizza.prototype.selectSize = function() {
+
+}
+
+Pizza.prototype.selectCrust = function() {
+
+}
+
+Pizza.prototype.selectSauce = function() {
+
+}
+
+Pizza.prototype.selectToppings = function() {
+
+}
+
+Order.prototype.calculatePrice = function () {
+
+};
+
+//Front End Logic
