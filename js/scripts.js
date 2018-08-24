3a9cdee012869e12221e28794521c749f7a083cf
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-24 12:22:26 -0700

    add basic back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,17 @@ var Toppings = [
 ]
 
 function Pizza() {
-  this.size = NaN;
-  this.crust = "";
+  this.size = "";
+  this.crust = ""
+  this.sauce = ""
   this.toppings = [];
+  this.sizeCost = NaN;
+  this.sizeMultiplier = NaN;
+  this.crustAddCost = 0;
   this.extraSauce = false;
+  this.extraCheese = 0;
+  this.noCheese = false;
+  this.toppingMultipliers = [];
 };
 
 function Order() {
@@ -29,24 +36,72 @@ function Order() {
   this.delivery = false;
 };
 
-Pizza.prototype.selectSize = function() {
+Pizza.prototype.selectSize = function(value) {
+  if (value === 1) {
+    this.size = "sm";
+    this.sizeCost = 10;
+    this.sizeMultiplier = 0.75;
+  } else if (value === 2) {
+    this.size = "md";
+    this.sizeCost = 13;
+    this.sizeMultiplier = 1;
+  } else if (value === 3) {
+    this.size = "lg";
+    this.sizeCost = 16;
+    this.sizeMultiplier = 1.5;
+  } else if (value === 4){
+    this.size = "xl"
+    this.sizeCost = 20;
+    this.sizeMultiplier = 2;
+  }
+};
 
-}
+Pizza.prototype.selectCrust = function(value) {
+  if (value === 1) {
+    this.crust = "thrown";
+  } else if (value === 2) {
+    this.crust = "thin";
+  } else if (value === 3) {
+    this.crust = "deep";
+    this.crustAddCost = 2;
+  } else if (value === 4) {
+    this.crust = gf;
+    this.crustAddCost = 3;
+  }
+};
 
-Pizza.prototype.selectCrust = function() {
+Pizza.prototype.selectSauce = function(boolean) {
+  if (boolean === true) {
+    extraSauce = true;
+  }
+};
 
-}
+Pizza.prototype.selectExtraCheese = function(value) {
+  if (value === 2) {
+    this.extraCheese = 1;
+  } else if (value === 3) {
+    this.extraCheese = 2;
+  } else if (value === 4) {
+    this.noCheese = true;
+  }
+};
 
-Pizza.prototype.selectSauce = function() {
+Pizza.prototype.selectToppings = function(array) {
+  this.toppings.push(array[0]);
+  this.toppingMultipliers.push(array[0]);
+};
 
-}
+Order.prototype.multiplyToppings = function(array, pizzaObj) {
+  var array = pizzaObj.toppings;
+  var toppingTotal = array.map(function(array, pizzaObj) {
+    var cost = array[1];
+    var size = pizzaObj.sizeMultiplier;
+    return cost * size;
+  });
+};
 
-Pizza.prototype.selectToppings = function() {
-
-}
-
-Order.prototype.calculatePrice = function () {
-
+Order.prototype.calculatePrice = function (pizzaObj) {
+  this.price = (pizzaObj.sizeCost + )
 };
 
 
